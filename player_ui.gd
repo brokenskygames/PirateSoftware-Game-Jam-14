c90c19b35214013273d235be3cd992d025d672b4
GoodGameJam2024 <gamejamgoodg@gmail.com>
2024-01-26 17:00:12 -0500

    Added UI Elements
    UI Added

---
+++ player_ui.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _ready():
+	$Baseplate.visible = true
+	$Shotgun.visible = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _shotgun():
+	$Shotgun.visible = true
+	
+func _sniper():
+	$Sniper.visible = true
